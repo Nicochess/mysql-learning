03c1b5fc2d3f798f08363dae471fe1dc452a01e8
Nicolas Magno <66505477+Nicochess@users.noreply.github.com>
2022-03-13 17:09:20 -0300

    feat: insert into model and select model

--- database_creation.sql
+++ database_creation.sql
@@ -25,3 +25,23 @@ CREATE TABLE pessoas(
     nacionalidade varchar(20) DEFAULT 'Brasil',
     PRIMARY KEY (id)
 ) DEFAULT CHARSET = utf8;
+
+
+/*
+    This is a model to insert data in a selected fields in a table 'pessoas'.
+    First you have to select the fields and then put the values
+    in the next line.
+
+    DO NOT INSERT ID AS A VALUE, BECAUSE IS AUTO_INCREMENTED. :D
+*/
+
+INSERT INTO pessoas
+(nome, nascimento, sexo, altura, peso, nacionalidade)
+VALUES
+('Nicolas', '2001-10-10', 'M', '1.81', '64.5', 'Brasil');
+
+/*
+    This command will show all the data in pessoas table
+*/
+
+select * from pessoas;
